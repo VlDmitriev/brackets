94f34dd20f465368979b86a60dcfa1d0d54329f0
VlDmitriev <dmitriev.vlad98@yandex.ru>
2022-02-23 22:42:17 +0400

    finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let openBracketsCon = '';
+    let closeBracketsCon = '';
+    let string = ''
+    for (let i in bracketsConfig){
+        //console.log(bracketsConfig[i][0])
+        openBracketsCon = openBracketsCon + bracketsConfig[i][0];
+        closeBracketsCon = closeBracketsCon + bracketsConfig[i][1];
+    }
+    //console.log(openBracketsCon)
+    //console.log(closeBracketsCon)
+    for (let i in str){
+      let index = closeBracketsCon.indexOf(str[i])
+      //console.log(index)
+      if (index != -1){
+          //console.log(index)
+          if (string[string.length - 1] != openBracketsCon[index]){
+              return false
+          }else{
+              string = string.slice(0, -1)
+          }
+      }else{
+          string = string + str[i]
+      }
+    }
+    //console.log(string)
+    if(string === ''){
+        return true
+    }else{
+        return false
+    }
 }
